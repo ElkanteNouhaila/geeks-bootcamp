0427c51804e8478e081d8619506f2f6c94fa18f2
Nouhaila El Kante <nouhailaelkante2@gmail.com>
2026-02-12 15:05:37 +0100

    merge words

--- week_2/day_1/exercices_xp_ninja.js
+++ week_2/day_1/exercices_xp_ninja.js
@@ -1,7 +1,5 @@
-mergeWords('Hello')();
-
-mergeWords('There')('is')('no')('spoon.')();
-
-
 const mergeWords = (word) => (next) =>
   next === undefined ? word : mergeWords(word + ' ' + next);
+
+console.log(mergeWords('Hello')());
+console.log(mergeWords('There')('is')('no')('spoon.')());
